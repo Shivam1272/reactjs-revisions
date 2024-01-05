b85a8aaa52e668544388a8d6f7b515187b5f0996
Shivam <sspatel120702@gmail.com>
2024-01-05 19:05:24 +0530

    setup tailwind & header , footer

--- 12blog/src/App.jsx
+++ 12blog/src/App.jsx
@@ -1,7 +1,34 @@
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
 import "./App.css";
+import { Footer, Header } from "./compontents";
 
 function App() {
-  return <div>A Blog Application with Appwrite</div>;
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, [dispatch]);
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>TODO</main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;
